24516ec07960ba143cfebbcc1c2409eef1dbafdc
benjaminottewell <benjaminottewell@outlook.com>
2024-08-18 22:36:06 -0700

    upload commit

---
+++ uk_data_analyst_jobs_project_sql/1_highest_paying_jobs.sql
@@ -0,0 +1,20 @@
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM 
+    job_postings_fact
+LEFT JOIN
+    company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    (job_title_short = 'Data Analyst' OR job_title_short = 'Business Analyst') AND
+    (job_location LIKE '%, UK%' OR job_location LIKE '%, United Kingdom%') AND
+    salary_year_avg IS NOT NULL
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 
+    10;
---
+++ uk_data_analyst_jobs_project_sql/2_highest_paying_jobs_with_skills.sql
@@ -0,0 +1,36 @@
+WITH top_paying_jobs_uk AS (
+    SELECT
+        job_id,
+        job_title,
+        job_title_short,
+        job_location,
+        salary_year_avg,
+        name AS company_name
+    FROM 
+        job_postings_fact
+    LEFT JOIN
+        company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        (job_title_short = 'Data Analyst' OR job_title_short = 'Business Analyst') AND
+        (job_location LIKE '%, UK%' OR job_location LIKE '%, United Kingdom%') AND
+        salary_year_avg IS NOT NULL
+    ORDER BY 
+        salary_year_avg DESC
+    LIMIT 
+        10
+)
+SELECT 
+    top_paying_jobs_uk.*,
+    STRING_AGG(skills_dim.skills, ', ') AS skills -- Use GROUP_CONCAT for MySQL
+FROM top_paying_jobs_uk
+INNER JOIN skills_job_dim ON top_paying_jobs_uk.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+GROUP BY 
+    top_paying_jobs_uk.job_id,
+    top_paying_jobs_uk.job_title,
+    top_paying_jobs_uk.job_title_short,
+    top_paying_jobs_uk.job_location,
+    top_paying_jobs_uk.salary_year_avg,
+    top_paying_jobs_uk.company_name
+ORDER BY
+    salary_year_avg DESC;
