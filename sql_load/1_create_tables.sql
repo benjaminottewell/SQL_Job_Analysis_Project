5af570494c5a3a35d421f257c5f5280935de753f
benjaminottewell <benjaminottewell@outlook.com>
2024-08-22 20:56:53 -0700

    Upload Changes

--- sql_load/1_create_tables.sql
+++ sql_load/1_create_tables.sql
@@ -1,4 +1,3 @@
--- Create company_dim table with primary key
 CREATE TABLE public.company_dim
 (
     company_id INT PRIMARY KEY,
@@ -8,7 +7,6 @@ CREATE TABLE public.company_dim
     thumbnail TEXT
 );
 
--- Create skills_dim table with primary key
 CREATE TABLE public.skills_dim
 (
     skill_id INT PRIMARY KEY,
@@ -16,7 +14,6 @@ CREATE TABLE public.skills_dim
     type TEXT
 );
 
--- Create job_postings_fact table with primary key
 CREATE TABLE public.job_postings_fact
 (
     job_id INT PRIMARY KEY,
@@ -38,7 +35,7 @@ CREATE TABLE public.job_postings_fact
     FOREIGN KEY (company_id) REFERENCES public.company_dim (company_id)
 );
 
--- Create skills_job_dim table with a composite primary key and foreign keys
+
 CREATE TABLE public.skills_job_dim
 (
     job_id INT,
@@ -48,13 +45,11 @@ CREATE TABLE public.skills_job_dim
     FOREIGN KEY (skill_id) REFERENCES public.skills_dim (skill_id)
 );
 
--- Set ownership of the tables to the postgres user
 ALTER TABLE public.company_dim OWNER to postgres;
 ALTER TABLE public.skills_dim OWNER to postgres;
 ALTER TABLE public.job_postings_fact OWNER to postgres;
 ALTER TABLE public.skills_job_dim OWNER to postgres;
 
--- Create indexes on foreign key columns for better performance
 CREATE INDEX idx_company_id ON public.job_postings_fact (company_id);
 CREATE INDEX idx_skill_id ON public.skills_job_dim (skill_id);
 CREATE INDEX idx_job_id ON public.skills_job_dim (job_id);
